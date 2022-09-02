302e424621b4214fa8e660dbb7b63deeae94c4c4
boon2187 <fumiya.mai.ito@gmail.com>
2022-09-02 22:05:13 +0900

    ポケモンAPIを使ってポケモンデータを取得するためのざっくりとした形をつくる

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // ポケモンAPIのベースURL
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPlkemon();
+    };
+  }, []);
+
+
   return (
     <div className="App"></div>
   );
