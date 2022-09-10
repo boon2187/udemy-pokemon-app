c1ccb29dfb8ec5d411a18a5075bc4efe28dfd863
boon2187 <fumiya.mai.ito@gmail.com>
2022-09-11 07:51:17 +0900

    各ポケモンデータをCardコンポーネント（未定義）で表示するようにh1タグ部分を変更

--- src/App.js
+++ src/App.js
@@ -39,7 +39,11 @@ function App() {
         <h1>ロード中...</h1>
       ) : (
         <>
-          <h1>ポケモンデータを取得しました</h1>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />
+            })}
+          </div>
         </>
       )}
     </div>
